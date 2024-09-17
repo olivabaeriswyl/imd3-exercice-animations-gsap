f7586dd39f579ed4e52339b5f31a3ca11cefea12
olivabaeriswyl <122967424+olivabaeriswyl@users.noreply.github.com>
2024-09-17 17:13:23 +0200

    gg

--- src/js/main.js
+++ src/js/main.js
@@ -12,6 +12,8 @@
     durant 3 secondes
    ----------------------- */
 
+gsap.to("#js-exercise-1", { x: "-100px" });
+
 /* -----------------------
     Exercice 2
    -----------------------
@@ -19,6 +21,8 @@
     durant 2 secondes
     après un délai de 2 secondes
    ----------------------- */
+
+gsap.to("#js-exercise-2", { rotate: 45 });
 
 /* -----------------------
     Exercice 3
@@ -29,6 +33,8 @@
     après un délai de 1 secondes
    ----------------------- */
 
+gsap.to("#js-exercise-3", { scale: 0.75, opacity: 0.5, duration: 2, delay: 1 });
+
 /* -----------------------
     Exercice 4
    -----------------------
@@ -37,6 +43,13 @@
     durant 2 secondes
     après un délai de 1 secondes
    ----------------------- */
+
+gsap.from("#js-exercise-4", {
+  scale: 0.75,
+  opacity: 0.5,
+  duration: 2,
+  delay: 1,
+});
 
 /* -----------------------
     Exercice 5 (timeline)
@@ -49,6 +62,12 @@
     durant 2 secondes après un délai de 1 seconde
    ----------------------- */
 
+const tl = gsap.timeline();
+
+tl.to("#js-exercise-5", { x: "-100px", duration: 3 });
+tl.to("#js-exercise-5", { rotate: 45, duration: 2, delay: 1 });
+tl.to("#js-exercise-5", { y: "-100px", duration: 2, delay: 1 });
+
 /* -----------------------
     Exercice 6 (timeline)
    -----------------------
@@ -57,6 +76,10 @@
     ET SIMULTANEMENT changer l'item 6 d'échelle (75%)
     durant 5 secondes
    ----------------------- */
+const tlTwo = gsap.timeline();
+
+tlTwo.to("#js-exercise-6", { y: "100px", duration: 3 });
+tlTwo.to("#js-exercise-6", { scale: 0.75, duration: 5 }, 0);
 
 /* -----------------------
     Exercice 7 (repeat + yoyo)
@@ -67,9 +90,21 @@
     et répéter ce mouvement à l'infini
    ----------------------- */
 
+gsap.to("#js-exercise-7", {
+  rotate: 135,
+  duration: 2,
+  repeat: "-1",
+  ease: "elastic.out",
+  yoyo: true,
+});
+
 /* -----------------------
     Exercice 8
    -----------------------
     Réaliser une animation libre
     lorsque le bouton est cliqué
    ----------------------- */
+
+gsap.to("#js-exercise-8", { scale: 1.2, duration: 1, ease: "back.inOut(4)" });
+
+import { gsap } from "gsap";
